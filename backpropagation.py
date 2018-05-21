5aad8303b66029715d644db2567f76d49e198aef
Diego Charrez <dcharrezt@unsa.edu.pe>
2018-05-20 22:00:25 -0500

    reworked network format

--- backpropagation.py
+++ backpropagation.py
@@ -20,34 +20,33 @@ def initialize_weights(n_inputs, n_hidden_layers, n_outputs):
 	network = []
 	neurons_per_layer = list( n_hidden_layers )
 	neurons_per_layer = [n_inputs] + neurons_per_layer + [n_outputs]
-		
+	
 	for i in range( len(neurons_per_layer) - 1 ):
-		network.append( np.random.rand( neurons_per_layer[i+1], \
-					neurons_per_layer[i] + 1 ) ) 
-
+		layer = []
+		for j in range( neurons_per_layer[i+1] ):
+			layer.append({'weights':[ random.random() for i \
+								in range ( neurons_per_layer[i] + 1 ) ]})
+		network.append( layer )
 	return network
 
 def forward_propagation( X_train , network ):
-
 	X_train_wbias = list(X_train) + [1]
-	tmp_out = [] 
 	for layer in network:
-		tmp_out = [] 
 		print("layer\t", layer)
 		for neuron in layer:
 			print("neuron\t", neuron)
 			print("X\t", X_train_wbias)
-			tmp_out.append( np.dot( X_train_wbias, neuron ) )
-		tmp_out = [ sigmoid(i) for i in tmp_out]
-		X_train_wbias = list(tmp_out) + [1]
-
-	print("TMP", tmp_out)
+			neuron['output'] = sigmoid(np.dot( X_train_wbias, neuron['weights']))
+		X_train_wbias = [i['output'] for i in layer] + [1]
 
 def square_error( target_output, output ):
 	error = 0.
 	for i in range( len(output) ):
 		error += 0.5 * ( ( target_output[i] - output[i] ) ** 2 )
 	return error
+
+# def backward_propagation(  ):
+
 
 if __name__ == "__main__":
 
@@ -59,6 +58,7 @@ if __name__ == "__main__":
 	network = initialize_weights( n_inputs, n_hidden_layers, n_outputs)
 	print(network)
 	forward_propagation( [1,1], network )
+	print( network )
 	# print(y_train)
 	# print(len(y_train))
 	# print("asdasdmflgsflgsdfijsndmfkoshdjfknglsdffl")
