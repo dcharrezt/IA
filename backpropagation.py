8993667e43b1ebffb1271f5955365a53dd5e77af
Diego Charrez <dcharrezt@unsa.edu.pe>
2018-05-22 15:16:42 -0500

    corrected bp and weight update

--- backpropagation.py
+++ backpropagation.py
@@ -5,17 +5,17 @@ from sklearn.model_selection import train_test_split
 from sklearn import datasets
 # from sklearn import preproccesing
 
-n_inputs = 2
-n_hidden_layers = [3, 2]
-n_outputs = 2
-
+n_inputs = 4
+n_hidden_layers = [4, 4]
+n_outputs = 3
+n_epochs = 500
 learning_rate = 0.5
 
 def sigmoid( x ):
-	return 1 / ( 1 + math.exp(-x) )
+	return 1. / ( 1. + math.exp(-x) )
 
 def sigmoid_derivative( x ):
-	return x*(1-x)
+	return x * ( 1. - x )
 
 def initialize_weights(n_inputs, n_hidden_layers, n_outputs):
 	network = []
@@ -36,6 +36,7 @@ def forward_propagation( X_train , network ):
 		for neuron in layer:
 			neuron['output'] = sigmoid(np.dot( current_input, neuron['weights']))
 		current_input = [i['output'] for i in layer] + [1]
+	return current_input
 
 def square_error( target_output, output ):
 	error = 0.
@@ -44,27 +45,41 @@ def square_error( target_output, output ):
 	return error
 
 def backward_propagation( network, y_train ):
-	for layer, next_layer in zip(reversed(network[:-1]), reversed(network)):
+	for i in reversed( range(len(network)) ):
 		errors = []
-		if next_layer == network[-1]:
-			for neuron, target in zip(layer, y_train):
-				errors.append(  neuron['output'] - target )
-		else:
-			for i in range( len(layer) ):
+		if i != len(network)-1 :
+			for j in range( len(network[i]) ):
 				err = 0.
-				for next_neuron in next_layer:
-					err += ( next_neuron['weights'][i] * next_neuron['delta'] )
+				for neuron in network[i+1]:
+					err += ( neuron['weights'][j] * neuron['delta'] )
 				errors.append( err )
-		for neuron, error in zip(layer, errors):
-			neuron['delta'] = error * sigmoid_derivative(neuron['output'])
+		else:
+			for j in range( len(network[i]) ):
+				errors.append(  y_train[j] - network[i][j]['output'] )
+		for j in range( len(network[i]) ):
+			network[i][j]['delta'] = errors[j] * \
+							sigmoid_derivative( network[i][j]['output'])
 
 def update_weights( network, inputs ):
-	for layer, next_layer in zip(network, network[1:]):
-		for neuron in layer:
-			for i in range( len( inputs) ):
-				neuron['weights'][i]+= learning_rate*neuron['delta']*inputs[i]
+	for i in range( len(network) ):
+		if i != 0:
+			inputs = [ neuron['output'] for neuron in network[i-1] ]
+		for neuron in network[i]:
+			for j in range( len( inputs) ):
+				neuron['weights'][j] += learning_rate*neuron['delta']*inputs[j]
 			neuron['weights'][-1] += learning_rate*neuron['delta']
-		inputs = [ neuron['output'] for neuron in next_layer ]
+
+def train_network( network, X_train, y_train, n_epochs ):
+	for i in range( n_epochs ):
+		error = 0.
+		for inputs, out in zip(X_train, y_train):
+			outputs = forward_propagation( inputs, network )
+			target = [0] *  n_outputs 
+			target[ out ] = 1
+			error += square_error( target, outputs[:-1] )
+			backward_propagation( network, target )
+			update_weights( network, inputs )
+		print("Epoch: "+str(i)+"\tError: "+str(error))
 
 if __name__ == "__main__":
 
@@ -73,20 +88,9 @@ if __name__ == "__main__":
 	X_train, X_test, y_train, y_test = train_test_split(
 				iris.data, iris.target, test_size=0.4, random_state=0)
 
+	# print(y_train)
+
 	network = initialize_weights( n_inputs, n_hidden_layers, n_outputs)
-	print(network)
-	forward_propagation( [1,1], network )
-	print( network )
-	backward_propagation( network, [0, 1] )
-	print( network )
-	update_weights( network, [1, 1] )
-	# print(y_train)
-	# print(len(y_train))
-	# print("asdasdmflgsflgsdfijsndmfkoshdjfknglsdffl")
-	# print(y_test)
-
-	# print(X_train)
-	# print(len(X_train))
-
-	# print(y_train)
-	# print(len(y_train))
+	# outputs = forward_propagation( X_train, network )
+	# backward_propagation( network, [0,0,1] )
+	train_network( network, X_train, y_train, n_epochs )
