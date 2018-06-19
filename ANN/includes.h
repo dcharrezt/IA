e72231a650b2dd902109dbda84779c425d15af6c
Diego Charrez <dcharrezt@unsa.edu.pe>
2018-06-19 08:51:17 -0500

    tested with iris dataset

--- ANN/includes.h
+++ ANN/includes.h
@@ -6,6 +6,13 @@
 #include <cstdlib>
 #include <cstring>
 #include <random>
+#include <fstream>
+
+#include <vector>
+#include <iterator>
+#include <string>
+#include <algorithm>
+#include <boost/algorithm/string.hpp>
 
 using namespace std;
 
